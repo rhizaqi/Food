6a756f63d926f60d03b049722ad77fccb70e8579
Oki <rhizaqi94@gmail.com>
2024-09-26 17:14:42 +0700

    feat: make seed and helper for hash w/ bcrypt

---
+++ server/seeders/20240926064932-seed-food.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    let data = require("../mySeeds/Food.json");
+
+    data = data.map((el) => {
+      el.updatedAt = el.createdAt = new Date();
+      return el;
+    });
+
+    await queryInterface.bulkInsert("Food", data, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Food", null, {});
+  },
+};
