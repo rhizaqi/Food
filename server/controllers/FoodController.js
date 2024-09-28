3848678908d01eda37eb4e2f9de962b8fc6d8bbc
Oki <rhizaqi94@gmail.com>
2024-09-28 13:18:46 +0700

    feat: success seed categories and food

---
+++ server/controllers/FoodController.js
@@ -0,0 +1,11 @@
+module.exports = class FoodController {
+    static async getFoods (req, res) {
+        try {
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    
+}
