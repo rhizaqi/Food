82b2b865bd08063eee33cc87e05b8ae2376b42e6
Oki <rhizaqi94@gmail.com>
2024-10-18 19:03:11 +0700

    feat: try figuring out for authorization

---
+++ server/middleware/authorization.js
@@ -0,0 +1,10 @@
+async function authorization (req, res) {
+    try {
+        console.log('masuk di authorization');
+        
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = authorization
