cc8a2d52fcb9a7789a76e78def5a6bde20ed6f26
Oky <146697026+rhizaqi@users.noreply.github.com>
2024-11-11 08:49:04 +0700

    feat: layout main page and card

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -2,45 +2,26 @@ export default function Card() {
   //bisa fetch atau turunan dari page lain
 
   return (
-    <div className="p-10">
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img
-            className="rounded-t-lg"
-            src="https://images.unsplash.com/photo-1729095249355-c7fdaf806d1d?q=80&w=1576&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
-        </a>    
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+    <div className="flex justify-center p-10 relative">
+      <div className="flex items-center">
+        <img
+          className="flex w-28 h-28 object-cover rounded-full absolute"
+          src="https://images.unsplash.com/photo-1549611016-3a70d82b5040?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          style={{
+            top: "30%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }} // Center the image
+        />
+      </div>
+      <div className="flex justify-center mt-20 rounded-2xl w-48 h-60 bg-red-400">
+        <div className="flex flex-col mt-20 gap-2 text-xl ">
+          <div className="flex flex-row gap-5 font-bold">
+            <div>Burger</div>
+            <div>Price</div>
+          </div>
+          <div className="flex justify-center">Description</div>
         </div>
       </div>
     </div>
--- client/src/pages/mainPage.jsx
+++ client/src/pages/mainPage.jsx
@@ -1,11 +1,17 @@
-export default function MainPage(){
-    return (
-        <div className="flex justify-center bg-red-100 w-screen h-screen">
-            <div className="flex justify-center bg-white m-10 w-11/12 rounded-3xl ">
-                <div className="flex bg-red-300 m-10">
-                    ini banner
-                </div>
-            </div>
+export default function MainPage() {
+  return (
+    <div className="flex justify-center bg-red-100 w-screen h-screen">
+      <div className="flex justify-center bg-white m-10 w-11/12 rounded-3xl ">
+        <div className="flex flex-col w-3/4 p-10">
+          <div>ini burger; search; profile; cart</div>
+          <div>Banner</div>
+          <div>Categories</div>
+          <div>Menus</div>
         </div>
-    )
+        <div className="flex bg-blue-200 rounded-3xl w-1/4">
+          <div className="p-10">My Cart</div>
+        </div>
+      </div>
+    </div>
+  );
 }
