cb1d8ed30ae0ccf6352edfdb112cd2ec3682a3f8
Oki <rhizaqi94@gmail.com>
2024-11-21 15:38:19 +0700

    feat: button and card

--- client/src/components/button.jsx
+++ client/src/components/button.jsx
@@ -1,11 +1,7 @@
-export default function Button() {
-  // ini button nya yahh
-  return (
-    <button
-      type="button"
-      class="text-white bg-green-800 hover:bg-green-500 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-2.5 py-2.5 me-2 mb-2"
-    >
-      Add to Chart
-    </button>
-  );
+export default function Button (){
+    return(
+        <div>
+            ini button
+        </div>
+    )
 }
