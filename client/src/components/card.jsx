a545420adc209a9cde26ab4813032948e30623ca
Oki <rhizaqi94@gmail.com>
2024-11-21 14:07:07 +0700

    feat:create card

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,46 +1,25 @@
+import Button from "./button";
+
 export default function Card() {
   //bisa fetch atau turunan dari page lain
-
   return (
-    <div className="p-10">
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+    <div className="flex justify-center p-5">
+      <div className="flex flex-col gap-2 w-60 h-70 bg-blue-200 p-2 rounded-2xl">
+        <div>
           <img
-            className="rounded-t-lg"
-            src="https://images.unsplash.com/photo-1729095249355-c7fdaf806d1d?q=80&w=1576&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="w-60 h-40 object-cover rounded-t-xl"
+            src="https://images.unsplash.com/photo-1589627461407-6257b1acf0fd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
           />
-        </a>    
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+        </div>
+        <div className="text-center font-bold text-xl">Nama Menu</div>
+        <div className="w-50 h-20 break-words text-center">
+          Description sadagldhva sdasdavafwefutwajl sbvdaywy
+        </div>
+        <div className="p-2 flex flex-row justify-between">
+          <div className="text-xl">Rp Price</div>
+          {/* <div className="text-xl"> Button Buy </div> */}
+          <Button/>
         </div>
       </div>
     </div>
