eeede94b548cd5fc2ed9a0d29f9b45830a39b560
Oki <rhizaqi94@gmail.com>
2024-11-21 19:53:52 +0700

    feat: rollback button

--- client/src/components/button.jsx
+++ client/src/components/button.jsx
@@ -1,12 +1,7 @@
-export default function Button() {
-  return (
-    <div>
-      <button
-        type="button"
-        class="text-white bg-green-800 hover:bg-green-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-2.5 py-2.5 me-2 mb-2"
-      >
-        Add to Cart
-      </button>
-    </div>
-  );
+export default function Button (){
+    return(
+        <div>
+            ini button
+        </div>
+    )
 }
