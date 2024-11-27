e80d5dd2d20de1a648d79ad0554c7cd163895bec
Oky <146697026+rhizaqi@users.noreply.github.com>
2024-11-27 19:35:55 +0700

    feat: change data food so i can put category name to card

--- client/src/components/CardMenu.jsx
+++ client/src/components/CardMenu.jsx
@@ -1,5 +1,7 @@
-export default function CardMenu(props) {
+export default function CardMenu({props}) {
     console.log(props,`ini data menu`);
+
+    // console.log(props.Category,`ini category menu`);
     
   return (
     <div className="p-5 bg-white rounded-xl shadow-md overflow-hidden">
@@ -7,14 +9,14 @@ export default function CardMenu(props) {
         alt="classNameic Caesar Salad"
         className="w-full h-40 object-cover rounded-xl"
         height="400"
-        src={props.props.imgUrl}
+        src={props.imgUrl}
         width="600"
       />
       <div className="p-4">
-        <h3 className="text-lg font-semibold">{props.props.name}</h3>
+        <h3 className="text-lg font-semibold">{props.name}</h3>
         <p className="text-gray-500">
           <i className="text-yellow-500"></i>
-          4.5 • Deep Cafe • Salad
+          4.5 • {props.Category.nameCategory}
         </p>
       </div>
     </div>
--- server/controllers/FoodController.js
+++ server/controllers/FoodController.js
@@ -1,10 +1,19 @@
-const { Food } = require("../models/");
+const { Food, Category } = require("../models/");
 
 module.exports = class FoodController {
   static async getFoods(req, res) {
     try {
       //   console.log(req, `req ??`);
-      const data = await Food.findAll();
+      const data = await Food.findAll({
+        include: [{ model: Category }],
+      });
+
+      // const formattedData = data.map(food => {
+      //   return {
+      //     ...food.toJSON(),
+      //     categoryId: food.Category  // Replace categoryId with the full Category object
+      //   };
+      // });
 
       res.status(200).json(data);
     } catch (error) {
@@ -31,17 +40,19 @@ module.exports = class FoodController {
     try {
       const { name, price, description, categoryId } = req.body;
 
-       await Food.create({
-        name, price, description, categoryId
-      })
+      await Food.create({
+        name,
+        price,
+        description,
+        categoryId,
+      });
 
       res.status(201).json({
-        message:"New food just got added"
-      })
-      
+        message: "New food just got added",
+      });
     } catch (error) {
       console.log(error);
-      
+
       throw error;
     }
   }
@@ -50,21 +61,26 @@ module.exports = class FoodController {
     try {
       const { name, price, description, categoryId } = req.body;
 
-      const id = req.params
+      const id = req.params;
 
-      const putFood = await Food.update ({
-        name, price, description, categoryId
-      }, {
-        where: id
-      })
+      const putFood = await Food.update(
+        {
+          name,
+          price,
+          description,
+          categoryId,
+        },
+        {
+          where: id,
+        }
+      );
 
       res.status(201).json({
-        message:"Food updated successfully"
-      })
-
+        message: "Food updated successfully",
+      });
     } catch (error) {
       console.log(error);
-      
+
       throw error;
     }
   }
@@ -73,23 +89,23 @@ module.exports = class FoodController {
     try {
       const { id } = req.params;
 
-      const findFood = await Food.findByPk(id)
+      const findFood = await Food.findByPk(id);
 
-      if(!findFood){
+      if (!findFood) {
         throw {
-          name:"NotFound"
-        }
+          name: "NotFound",
+        };
       }
-      
+
       await Food.destroy({
         where: {
-          id
-        }
-      })
+          id,
+        },
+      });
 
       res.status(200).json({
-        message:`Food just deleted successfully`
-      })
+        message: `Food just deleted successfully`,
+      });
     } catch (error) {
       throw error;
     }
