30b8f58d5a1536c385872895c33454feafabc578
Oki <rhizaqi94@gmail.com>
2024-12-27 20:06:47 +0700

    feat: success routes login in client

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,24 +1,76 @@
-import { useState } from "react"
+import { useState } from "react";
+import axios from "../config/config.jsx";
+import { useNavigate } from "react-router-dom";
 
-import axios from "../config/config"
+export default function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-export default function Login (){
+  const moveToRegister = async () => {
+    navigate("/register");
+  };
 
-    const [user, setUser] = useState({})
+  const inputHandler = (event) => {
+    const { value, id } = event.target;
+    setUser({
+      ...user,
+      [id]: value,
+    });
+  };
 
-    const loginHandle = async () => {
-        try {
-           const goLogin = await axios('/login' + user)
+  const loginHandle = async (event) => {
+    event.preventDefault();
+    try {
+      const goLogin = await axios({
+        method: "post",
+        url: "/user/login",
+        data: user,
+      });
 
-
-        } catch (error) {
-            throw error
-        }
+      console.log(goLogin, `>> ??`);
+    } catch (error) {
+      console.log(error, `error login`);
+      throw error;
     }
+  };
 
-    return(
-        <div>
-            ini page login
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center p-5 bg-green-200 w-screen h-screen ">
+      <div>ini page login</div>
+      <form onSubmit={loginHandle}>
+        <div className="flex flex-col">
+          <label className="mt-5 mb-2"> Email </label>
+          <input
+            type="text"
+            id="email"
+            value={user.email}
+            onChange={(event) => inputHandler(event)}
+            className="p-2 bg-gray-400 w-30 h-10 rounded-xl"
+          />
         </div>
-    )
+        <div className="flex flex-col">
+          <label className="mt-5 mb-2"> Password </label>
+          <input
+            type="text"
+            id="password"
+            value={user.password}
+            onChange={(event) => inputHandler(event)}
+            className="p-2 bg-gray-400 w-30 h-10 rounded-xl"
+          />
+        </div>
+        <button type="submit" className="mt-2 w-20 h-10 bg-red-200 rounded-xl">
+          Login
+        </button>
+      </form>
+      {/* <button
+        onClick={() => moveToRegister()}
+        className="w-20 h-10 bg-red-200 rounded-xl"
+      >
+        Register
+      </button> */}
+    </div>
+  );
 }
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/login";
 import Register from "../pages/register";
+import MainPage2 from "../pages/mainPage2";
 
 const router = createBrowserRouter([
-    {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path: "/register",
-        element: <Register/>
-    }
-])
+  {
+    path: "/",
+    element: <MainPage2 />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
-export default router
+export default router;
